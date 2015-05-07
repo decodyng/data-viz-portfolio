cf75525b5e23d56baa2b4ef234eeb298a0560fb4
decodyng <cody.marie.wild@gmail.com>
2015-05-07 16:20:16 -0700

    no more tempting fate

---
+++ classYearTransform.py
@@ -0,0 +1,40 @@
+from __future__ import division
+import pandas as pd 
+from random import uniform 
+import json 
+
+def generateFakeContinents(): 
+	firstCont = round(uniform(0, 0.75), 3)
+	remainder = 1 - firstCont
+	secondCont = round(uniform(0, remainder), 3)
+	thirdCont = round(1 - (firstCont + secondCont), 3)
+	return [{'continent': 'MENA', 'percentage': firstCont},
+			{'continent': 'Asia', 'percentage': secondCont},
+			{'continent': 'Latin America', 'percentage': thirdCont}]
+
+if __name__ == "__main__":
+	df = pd.read_csv("classificationPercentByYear.csv")
+	outArray = []
+	for ind, row in df.iterrows(): 
+		rowDict = {}
+		print ind, row
+		if int(row['year']) < 1986: 
+			continue 
+		rowDict['year'] = int(row['year'])
+		ncables = int(row['totalcables'])
+		rowDict['nCables'] = ncables
+		rowDict['stackedData'] = {}
+		rowDict['stackedData']['secret'] = round(row['percsecret'], 3)*ncables
+		rowDict['stackedData']['confidential'] = round(row['percconfidential'], 3)*ncables
+		rowDict['stackedData']['unclassified'] = round(row['percunclassified'], 3)*ncables
+		rowDict['stackedData']['unknown'] = round(row['percentunknown'], 3)*ncables
+		rowDict['continentData'] = []
+		rowDict['continentData'].append({'classification': 'secret', 'continents': generateFakeContinents()})
+		rowDict['continentData'].append({'classification': 'confidential', 'continents':generateFakeContinents()})
+		rowDict['continentData'].append({'classification': 'unclassified', 'continents': generateFakeContinents()})
+		
+		outArray.append(rowDict)
+
+	with open('classifiedCountsByYear.json', 'wb') as fp:
+		json.dump(outArray, fp) 
+
